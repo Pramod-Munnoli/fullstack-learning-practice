953462ed25f5d1dd5bbf78b7743bc4fea78a5891
Pramod-munnoli <pramodmunnoli99@gmail.com>
2026-02-16 18:55:10 +0530

    First commit: React learning project with state hooks

---
+++ src/Counter.jsx
@@ -0,0 +1,16 @@
+import { useState} from "react";
+export default function Counter(){
+     let[Count , setCount]= useState(0);
+     let  incCount = () => {
+       setCount(Count+1);
+       console.log(Count)
+     }
+    
+
+    return( 
+        <div>
+            <h3>Count ={Count} </h3>
+            <button onClick={incCount}>Increment</button>
+        </div>
+    )
+} 
